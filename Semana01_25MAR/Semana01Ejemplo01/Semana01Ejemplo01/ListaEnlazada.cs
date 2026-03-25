b7b9044b6882cfdb3c98440241ccc34d2c1dcacf
Gustavo Coronel <gcoronelc@gmail.com>
2026-03-25 10:32:27 -0500

    Semana 1

---
+++ Semana01_25MAR/Semana01Ejemplo01/Semana01Ejemplo01/ListaEnlazada.cs
@@ -0,0 +1,46 @@
+﻿namespace Semana01Ejemplo01
+{
+    internal class ListaEnlazada
+    {
+        private Nodo Cabeza;
+
+        public ListaEnlazada()
+        {
+            Cabeza = null;
+        }
+
+        public void AgregarAlFinal(int dato)
+        {
+            Nodo nuevoNodo = new Nodo(dato);
+            if (Cabeza == null)
+            {
+                Cabeza = nuevoNodo;
+            }
+            else
+            {
+                Nodo actual = Cabeza;
+                while (actual.Siguiente != null)
+                {
+                    actual = actual.Siguiente;
+                }
+                actual.Siguiente = nuevoNodo;
+            }
+        }
+
+        public void Imprimir()
+        {
+            if (Cabeza == null)
+            {
+                Console.WriteLine("La lista está vacía.");
+                return; // Finaliza el programa
+            }
+            Nodo actual = Cabeza;
+            while (actual != null)
+            {
+                Console.WriteLine(actual.Dato);
+                actual = actual.Siguiente;
+            }
+        }
+
+    }
+}
